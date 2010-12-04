7297c8294debbf42189aee64092ff887b95772cb
Bart Massey <bart@cs.pdx.edu>
2010-12-03 17:37:10 -0800

    turned off reftex mode for now

--- emacs.el
+++ emacs.el
@@ -291,11 +291,11 @@
   (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
 
 ; RefTeX and AucTeX
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (load "auctex.el" nil t t)
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq-default TeX-master nil)
+;  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+;  (load "auctex.el" nil t t)
+;  (setq TeX-auto-save t)
+;  (setq TeX-parse-self t)
+;  (setq-default TeX-master nil)
 
 ; Page mode
   (autoload 'page-mode "page-mode" "Page-oriented display" t)
